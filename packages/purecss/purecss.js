77e96903afcb56890f336672dceb297d1045e452
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-08-12 17:40:18 +0700

    layout

---
+++ packages/purecss/purecss.js
@@ -0,0 +1,9 @@
+// Write your package code here!
+var head = document.getElementsByTagName('head')[0];
+
+var style = document.createElement('link');
+style.type = 'text/css';
+style.rel = "stylesheet";
+style.href = '//yui.yahooapis.com/pure/0.6.0/pure-min.css';
+
+head.appendChild(style);
